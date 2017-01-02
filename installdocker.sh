a2ba512c4aa50814052e366bdcffae6efef5c914
dalbymodo <dalbymodo@hotmail.se>
2017-01-02 16:40:44 +0100

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get install sudo
-apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv \
                --keyserver hkp://ha.pool.sks-keyservers.net:80 \
                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
